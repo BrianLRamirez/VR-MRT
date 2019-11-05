791198fd243ff2a83d13256732ad358ba4f4341d
Brian Ramirez <bramirez@Brians-MacBook-Pro.local>
2019-11-05 06:58:00 -0500

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour{
+    // Start is called before the first frame update
+    public GameObject box;
+
+    void Start(){
+        box = GameObject.Find("box");
+    }
+
+    // Update is called once per frame
+    void Update(){
+        float thumbstick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x;
+        box.transform.Rotate(0, (thumbstick * 10), 0);
+    }
+}
