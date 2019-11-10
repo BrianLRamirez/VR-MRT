c4d379cf1e9b56606f075c71904b66dc84cfb863
Brian Ramirez <bramirez@gse.harvard.edu>
2019-11-09 20:13:59 -0500

    stable proof of concept

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour{
     public GameObject puzzleLeft;
     public GameObject puzzleRight;
     string[] puzzleList = {"puzzle1","puzzle2", "puzzle3"}; 
-    int currentPuzzle = 0;
+    int currentPuzzle = 1;
     Vector3 originLeft = new Vector3(1.14f,1.3f,1.84f);
     Vector3 originRight= new Vector3(1.14f,1.3f,-2.5f);
 
@@ -28,6 +28,8 @@ public class GameManager : MonoBehaviour{
         thumbstickX = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x;
         thumbstickY = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).y;
         updateDebugger();
+
+        handleRotation();
         
         if(OVRInput.Get(OVRInput.Button.One)){
         //    Do something
@@ -37,10 +39,11 @@ public class GameManager : MonoBehaviour{
     }
 
     void loadNewPuzzle(){
-        GameObject puzzleModel = Resources.Load(puzzleList[currentPuzzle]) as GameObject;
-        puzzleLeft =  GameObject.Instantiate(puzzleModel, originLeft, transform.rotation);
-        puzzleRight =  GameObject.Instantiate(puzzleModel, originRight, transform.rotation);
-        currentPuzzle++;
+        puzzleRight = GameObject.Find("puzzle1");
+        //Resources.Load(puzzleList[currentPuzzle]) as GameObject;
+        // puzzleLeft =  GameObject.Instantiate(puzzleModel, originLeft, transform.rotation);
+        // puzzleRight =  GameObject.Instantiate(puzzleModel, originRight, transform.rotation);
+        // currentPuzzle++;
     }
 
     void updateDebugger(){
@@ -50,6 +53,15 @@ public class GameManager : MonoBehaviour{
                             "<#3498db>Y: " + thumbstickY + "</color>\n";
         debuggerText.SetText(debugText);
     }
+
+     void handleRotation() {
+         if(OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight)|| OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft)){
+            puzzleRight.transform.localEulerAngles = new Vector3(puzzleRight.transform.localEulerAngles.x, puzzleRight.transform.localEulerAngles.y + (thumbstickX * 5), puzzleRight.transform.localEulerAngles.z);
+         } else if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp)|| OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown)) {
+             puzzleRight.transform.Rotate((thumbstickY * 5), 0 , 0);
+           
+         }
+     }
 
 
 }
