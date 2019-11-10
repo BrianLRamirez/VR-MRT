03769c94c48d2463d253804b61ec1aaf17ddd978
Brian Ramirez <bramirez@gse.harvard.edu>
2019-11-10 12:17:57 -0500

    isolated rotation func to its own script and added winning condition- still needs work

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,61 +8,75 @@ public class GameManager : MonoBehaviour{
     // Start is called before the first frame update
     public GameObject puzzleLeft;
     public GameObject puzzleRight;
-    string[] puzzleList = {"puzzle1","puzzle2", "puzzle3"}; 
+    public bool isGameOver;
+
+    public bool isPuzzleSolved;
+
+    string[] puzzleList = {"puzzle1","puzzle2"}; 
     int currentPuzzle = 0;
     Vector3 originRight = new Vector3(1.05f, 2.75f, -2.27f);
     Vector3 originLeft= new Vector3(1.05f, 2.75f, 1.45f);
 
     GameObject debugger;
 
-    float thumbstickX;
-    float thumbstickY;
-
     void Start(){
-        loadNewPuzzle();
+        loadPuzzle();
         debugger = GameObject.Find("Debugger");
+        isGameOver = false;
+        isPuzzleSolved = false;
     }
 
     // Update is called once per frame
     void Update(){
-        thumbstickX = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x;
-        thumbstickY = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).y;
         updateDebugger();
 
-        handleRotation();
+        checkForWinningCondition();
         
         if(OVRInput.GetDown(OVRInput.Button.One)){
-            loadNewPuzzle();
+            loadPuzzle();
         } else if (OVRInput.Get(OVRInput.Button.Two)){
         //    Do something else
         }
     }
 
-    void loadNewPuzzle(){
+    void loadPuzzle(){
         Destroy(puzzleLeft);
         Destroy(puzzleRight);
         GameObject puzzleModel = Resources.Load(puzzleList[currentPuzzle]) as GameObject;
         puzzleLeft =  GameObject.Instantiate(puzzleModel, originLeft, transform.rotation);
+        puzzleLeft.transform.Rotate(Random.Range(-360.0f, 360.0f), Random.Range(-360.0f, 360.0f), 0);
         puzzleRight =  GameObject.Instantiate(puzzleModel, originRight, transform.rotation);
+        puzzleRight.AddComponent<Rotateable>();
         currentPuzzle++;
     }
 
     void updateDebugger(){
         TextMeshPro debuggerText = GameObject.Find("DebuggerText").GetComponent<TextMeshPro>();;
-        string debugText = "Thumbstick Data: \n" + 
-                            "<#00ff00>X: " + thumbstickX + "</color>\n" + 
-                            "<#3498db>Y: " + thumbstickY + "</color>\n";
+        string debugText = "Puzzle Left: \n" + 
+                            "<#00ff00>X: " + puzzleLeft.transform.rotation.x + "</color>\n" + 
+                            "<#3498db>Y: " + puzzleLeft.transform.rotation.y + "</color>\n" +
+                            "Puzzle Right: \n" + 
+                            "<#00ff00>X: " + puzzleRight.transform.rotation.x + "</color>\n" + 
+                            "<#3498db>Y: " + puzzleRight.transform.rotation.y + "</color>\n";
         debuggerText.SetText(debugText);
     }
 
-     void handleRotation() {
-         if(OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight)|| OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft)){
-             puzzleRight.transform.Rotate(0, (thumbstickX * 3), 0);
-         } else if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp)|| OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown)) {
-             puzzleRight.transform.Rotate((thumbstickY * 3), 0 , 0);
-           
-         }
-     }
+    void checkForWinningCondition(){
+        bool xMatches = isWithinTolerance(puzzleLeft.transform.rotation.x, Mathf.Abs(puzzleRight.transform.rotation.x), 0.1f);
+        bool yMatches = isWithinTolerance(puzzleLeft.transform.rotation.y, puzzleRight.transform.rotation.y, 0.1f);
+        bool zMatches = isWithinTolerance(puzzleLeft.transform.rotation.z, puzzleRight.transform.rotation.z, 0.1f);
 
+        if(xMatches && yMatches && zMatches) {
+            isPuzzleSolved = true;
+            Destroy(puzzleRight.GetComponent<Rotateable>());
+        }
+    }
+
+    bool isWithinTolerance(float value, float compareTo, float tolerance){
+        float ceiling = value + tolerance;
+        float floor = value - tolerance;
+
+        return (compareTo >= floor && compareTo <= ceiling);
+    }
 
 }
---
+++ Assets/Scripts/Rotateable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotateable : MonoBehaviour {
+    
+    void Start() {
+        
+    }
+
+    void Update() {
+        float thumbstickX = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x;
+        float thumbstickY = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).y;
+
+         if(OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight)|| OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft)){
+             gameObject.transform.Rotate(0, (thumbstickX * 2), 0);
+         } else if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp)|| OVRInput.Get(OVRInput.Button.SecondaryThumbstickDown)) {
+             gameObject.transform.Rotate((thumbstickY * 2), 0 , 0);
+         }
+    }
+}
