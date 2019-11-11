5c73d75c6602b6aa25314447acf5f38bec73123b
Brian Ramirez <bramirez@gse.harvard.edu>
2019-11-10 22:57:50 -0500

    figured out win condition

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,11 +19,18 @@ public class GameManager : MonoBehaviour{
 
     GameObject debugger;
 
+    AudioSource audioSource;
+
+    AudioClip successSound;
+
+
     void Start(){
         loadPuzzle();
         debugger = GameObject.Find("Debugger");
         isGameOver = false;
         isPuzzleSolved = false;
+        audioSource = gameObject.GetComponent<AudioSource>();
+        successSound = (AudioClip) Resources.Load("Sounds/success");;
     }
 
     // Update is called once per frame
@@ -42,6 +49,7 @@ public class GameManager : MonoBehaviour{
     void loadPuzzle(){
         Destroy(puzzleLeft);
         Destroy(puzzleRight);
+        isPuzzleSolved = false;
         GameObject puzzleModel = Resources.Load(puzzleList[currentPuzzle]) as GameObject;
         puzzleLeft =  GameObject.Instantiate(puzzleModel, originLeft, transform.rotation);
         puzzleLeft.transform.Rotate(Random.Range(0f, 360.0f), Random.Range(0f, 360.0f), 0);
@@ -51,22 +59,34 @@ public class GameManager : MonoBehaviour{
     }
 
     void updateDebugger(){
+        bool xMatches = isWithinTolerance(puzzleLeft.transform.rotation.x, puzzleRight.transform.rotation.x, 0.06f);
+        bool yMatches = isWithinTolerance(puzzleLeft.transform.rotation.y, puzzleRight.transform.rotation.y, 0.06f);
+        bool zMatches = isWithinTolerance(puzzleLeft.transform.rotation.z, puzzleRight.transform.rotation.z, 0.07f);
+
         TextMeshPro debuggerText = GameObject.Find("DebuggerText").GetComponent<TextMeshPro>();;
         string debugText = "Puzzle Left: \n" + 
                             "<#00ff00>X: " + puzzleLeft.transform.rotation.x + "</color>\n" + 
                             "<#3498db>Y: " + puzzleLeft.transform.rotation.y + "</color>\n" +
+                            "<#ff46a5>Z: " + puzzleLeft.transform.rotation.z + "</color>\n" +
                             "Puzzle Right: \n" + 
                             "<#00ff00>X: " + puzzleRight.transform.rotation.x + "</color>\n" + 
-                            "<#3498db>Y: " + puzzleRight.transform.rotation.y + "</color>\n";
+                            "<#3498db>Y: " + puzzleRight.transform.rotation.y + "</color>\n" +
+                            "<#ff46a5>Z: " + puzzleRight.transform.rotation.z + "</color>\n" +
+                            "<#00ff00>X Matches: " + xMatches + "</color>\n" +
+                            "<#3498db>Y Matches: " + yMatches + "</color>\n" +
+                            "<#ff46a5>Z Matches: " + zMatches + "</color>\n";
         debuggerText.SetText(debugText);
     }
 
     void checkForWinningCondition(){
-        bool xMatches = isWithinTolerance(puzzleLeft.transform.rotation.x, puzzleRight.transform.rotation.x, 0.1f);
-        bool yMatches = isWithinTolerance(puzzleLeft.transform.rotation.y, puzzleRight.transform.rotation.y, 0.1f);
-        bool zMatches = isWithinTolerance(puzzleLeft.transform.rotation.z, puzzleRight.transform.rotation.z, 0.1f);
+        bool xMatches = isWithinTolerance(puzzleLeft.transform.rotation.x, puzzleRight.transform.rotation.x, 0.06f);
+        bool yMatches = isWithinTolerance(puzzleLeft.transform.rotation.y, puzzleRight.transform.rotation.y, 0.06f);
+        bool zMatches = isWithinTolerance(puzzleLeft.transform.rotation.z, puzzleRight.transform.rotation.z, 0.07f);
 
         if(xMatches && yMatches && zMatches) {
+            if(!isPuzzleSolved){
+                audioSource.PlayOneShot(successSound);
+            }
             isPuzzleSolved = true;
             Destroy(puzzleRight.GetComponent<Rotateable>());
         }
